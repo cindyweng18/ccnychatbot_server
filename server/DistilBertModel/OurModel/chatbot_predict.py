53461ea9f9ec4c9a2bb4d4c6fba7a9f72e7613f3
cindyweng18 <cindyweng18@gmail.com>
2021-11-19 23:03:09 -0500

    save label encoder variable to use in chatbot predict file

--- server/DistilBertModel/OurModel/chatbot_predict.py
+++ server/DistilBertModel/OurModel/chatbot_predict.py
@@ -5,42 +5,40 @@ import json
 import random
 import numpy as np
 from transformers import DistilBertTokenizer
-from sklearn.preprocessing import LabelEncoder
-from BERT_model import BERT_Model
-import pandas as pd
 
 
 loaded_model = pickle.load(open("model.sav", 'rb'))
-data = json.load(open("answers.json"))
+data = json.load(open("../data/answers.json"))
 
 def get_prediction(str, model):
-    df = pd.read_csv("test_chatbot.csv")
-    device = torch.device("cpu")
-    le = LabelEncoder()
-    le.fit_transform(df['Label'])
-    tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
-    str = re.sub(r'[^a-zA-Z ]+', '', str)
-    test_text = [str]
-    model.eval()
+  device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    tokens_test_data = tokenizer(
-    test_text,
-    padding=True,
-    truncation=True,
-    return_token_type_ids=False
-    )
-    test_seq = torch.tensor(tokens_test_data['input_ids'])
-    test_mask = torch.tensor(tokens_test_data['attention_mask'])
+  #cindy: load label encoder from chatbot.py file
+  le = pickle.load(open("label_encoder.pkl", 'rb'))
 
-    #   result = None
-    with torch.no_grad():
-        preds = model(test_seq.to(device), test_mask.to(device))
-        preds = preds.detach().cpu().numpy()
-        # result = preds[0]
-        preds_index = np.argmax(preds, axis = 1)
+  tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
+  str = re.sub(r'[^a-zA-Z ]+', '', str)
+  test_text = [str]
+  model.eval()
 
-    print("Intent Identified: ", le.inverse_transform(preds_index)[0])
-    return le.inverse_transform(preds_index)[0]
+  tokens_test_data = tokenizer(
+  test_text,
+  padding=True,
+  truncation=True,
+  return_token_type_ids=False
+  )
+  test_seq = torch.tensor(tokens_test_data['input_ids'])
+  test_mask = torch.tensor(tokens_test_data['attention_mask'])
+
+  #   result = None
+  with torch.no_grad():
+      preds = model(test_seq.to(device), test_mask.to(device))
+      preds = preds.detach().cpu().numpy()
+      # result = preds[0]
+      preds_index = np.argmax(preds, axis = 1)
+
+  print("Intent Identified: ", le.inverse_transform(preds_index)[0])
+  return le.inverse_transform(preds_index)[0]
 
 def get_response(message, loaded_model): 
   intent = get_prediction(message, loaded_model)
