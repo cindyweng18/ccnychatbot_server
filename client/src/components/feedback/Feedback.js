f93f68f53158a374cf71530b3f9c3fe9b0f79d94
Nahin Imtiaz <nimtiaz000@citymail.cuny.edu>
2021-11-21 19:00:04 -0500

    Added Feedback and RespondAnimation components

---
+++ client/src/components/feedback/Feedback.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Feedback.css'
+
+import ThumbDownOffAltIcon from '@mui/icons-material/ThumbDownOffAlt';
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import Button from '@mui/material/Button'
+
+
+const Feedback = (props)=>{
+
+    const {handleFeedback } = props
+
+    const handleClick =(e)=>{
+        //TODO: once clicked, check if its up or down. If its down, save the question to database. Close the feedback component as well.
+        if(e.currentTarget.value=="down"){
+            handleFeedback()
+            //close the component. I will do that later
+        }
+        else {
+            //close the component. I will do that later
+        }
+    }
+    return(
+        <div className='text'>
+            <span>
+                <p>
+                    Was this answer helpful?
+                </p>
+                <div>
+                    <Button onClick={handleClick} value="up">
+                        <ThumbUpOffAltIcon className='green'/>
+                    </Button>
+                    <Button onClick={handleClick} value="down">
+                        <ThumbDownOffAltIcon className='red'/>
+                    </Button>
+                </div>
+            </span>
+        </div>
+    )
+}
+export default Feedback
---
+++ client/src/components/respondAnimation/RespondAnimation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './RespondAnimation.css'
+
+const RespondAnimation = ()=>{
+
+    return(
+        <div className='chat-res'>
+            <div className='loading'>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    )
+}
+export default RespondAnimation
