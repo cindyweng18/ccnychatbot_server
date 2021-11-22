f7b94cf957a31ba32d6770a0bd94e85d4c4d6496
David Jimenez <djimene005@citymail.cuny.edu>
2021-11-21 19:09:51 -0500

    Post last message to messagelist on thumbs down

--- client/src/App.js
+++ client/src/App.js
@@ -1,80 +1,113 @@
-import React, {useState, useEffect,useRef } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
 
-import Container from '@mui/material/Container';
+import Container from "@mui/material/Container";
 
-import Header from './components/header/Header';
-import Chat from './components/chat/Chat';
-import MessageBox from './components/messageBox/MessageBox'
-import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import Header from "./components/header/Header";
+import Chat from "./components/chat/Chat";
+import MessageBox from "./components/messageBox/MessageBox";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import Feedback from "./components/feedback/Feedback";
 
-const App =()=> {
+const App = () => {
+  const [convo, setConvo] = useState([]);
 
-    const [convo, setConvo] = useState([])
+  const [botMessage, setBotMessage] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const [botMessage, setBotMessage] = useState ([])
-    const [isOpen, setIsOpen] = useState(false)
+  const ref = useRef();
 
-    const ref = useRef()
+  useEffect(async () => {
+    await axios.get("/chat/api/botmessage-list/").then(
+      (response) => response.data.map((m) => setBotMessage(m.value)),
+      (err) => console.error(err)
+    );
+  }, []);
 
-    useEffect (async () => {
-        await axios.get ('/chat/api/botmessage-list/')
-        .then (response => response.data.map (m => setBotMessage (m.value)),
-        (err) => console.error (err))
-    }, [])
+  // Fixes scroll issue at the bottom of conversation.
+  const messagesEndRef = useRef(null);
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+  useEffect(() => {
+    scrollToBottom();
+  });
 
-    // Fixes scroll issue at the bottom of conversation.
-    const messagesEndRef = useRef(null)
-    const scrollToBottom = () => {
-        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
+  const handleClick = async (e) => {
+    e.preventDefault();
+    //TODO: Get response from ML model using mes.
+
+    await axios
+      .get("/chat/api/botmessage-list/")
+      .then((response) => response.data.map((m) => setBotMessage(m.value)));
+    console.log(botMessage);
+
+    if (ref.current.value.length > 0) {
+      let newConvo = [...convo];
+      newConvo.push({ mes: ref.current.value, res: botMessage });
+      setConvo(newConvo);
+
+      // send the user message to the backend
+
+      // empty the text message field
+      ref.current.value = "";
     }
-    useEffect(() => {
-        scrollToBottom()
-    });
+  };
 
-    const handleClick = async (e) => {
-        e.preventDefault();
-        //TODO: Get response from ML model using mes.
-        
-        await axios.get ("/chat/api/botmessage-list/")
-        .then (response => response.data.map (m => setBotMessage (m.value)))
-        console.log (botMessage)
+  const handleFeedback = () => {
+    //Send last message to database
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "localhost:8000/chat/api/message-list/", true);
+    xhr.send(
+      JSON.stringify({
+        value: "Test string",
+      })
+    );
+  };
 
-        if(ref.current.value.length>0){
-            let newConvo= [...convo]
-            newConvo.push({mes:ref.current.value, res: botMessage})
-            setConvo(newConvo)
+  const openModal = () => {
+    setIsOpen(!isOpen);
+  };
 
-            // send the user message to the backend
-            
-
-            // empty the text message field
-            ref.current.value = ""
-        }
-    }
-
-    const openModal = ()=> {
-        setIsOpen(!isOpen)
-    }
-
-    return (
-        <>
-        {isOpen ?
-        <Container component="main" maxWidth="xs"style={{bottom:150,right:150,position:'absolute' }}>
-        <div style={{ boxShadow: '0px 0px 50px #a0a0a0', borderRadius: '1rem 1rem 1rem 1rem'}}>
-            <Header openModal={openModal}/>
-            <Chat convo={convo} ref={messagesEndRef}/>
-            <MessageBox handleClick={handleClick} ref={ref}/>
-        </div>
+  return (
+    <>
+      {isOpen ? (
+        <Container
+          component="main"
+          maxWidth="xs"
+          style={{ bottom: 150, right: 150, position: "absolute" }}
+        >
+          <div
+            style={{
+              boxShadow: "0px 0px 50px #a0a0a0",
+              borderRadius: "1rem 1rem 1rem 1rem",
+            }}
+          >
+            <Header openModal={openModal} />
+            <Chat convo={convo} ref={messagesEndRef} />
+            <Feedback handleFeedback={handleFeedback} />
+            <MessageBox handleClick={handleClick} ref={ref} />
+          </div>
         </Container>
-        
-        :
+      ) : (
         <div onClick={openModal}>
-        <ChatBubbleOutlineIcon sx={{ color: 'white',backgroundImage: 'linear-gradient(to right, #6e0fc8, #cd49e6)',fontSize:'5rem',borderRadius:'50%',padding:'2rem',bottom:50,right:50,position:'absolute',boxShadow: '0px 0px 50px #a0a0a0' }}/>
+          <ChatBubbleOutlineIcon
+            sx={{
+              color: "white",
+              backgroundImage: "linear-gradient(to right, #6e0fc8, #cd49e6)",
+              fontSize: "5rem",
+              borderRadius: "50%",
+              padding: "2rem",
+              bottom: 50,
+              right: 50,
+              position: "absolute",
+              boxShadow: "0px 0px 50px #a0a0a0",
+            }}
+          />
         </div>
-        }
-        </>
-    )
-}
+      )}
+    </>
+  );
+};
 
-export default App
+export default App;
